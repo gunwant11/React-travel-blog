36ee6d0f29e341f2f658d51a1fdaf3f93f948a7c
gunwant11 <gunwant7272@gmail.com>
2021-08-31 14:07:24 +0530

    layout fixed

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -3,13 +3,13 @@ import Author from "../Blog/Author";
 import BlogList from "../Blog/BlogList";
 import Footer from "./Footer";
 import Head from "./Head";
-import Panel from "./Panel";
+
 import classes from "./Landing.module.scss";
 const Landing = (props) => {
   return (
     <Fragment>
       <Head />
-      <Panel />
+
       <section className={classes.box}>
         <BlogList blogs={props.blogs} />
         <Author />
