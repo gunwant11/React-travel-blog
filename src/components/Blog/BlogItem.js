a7d50d11c4cd826b0909ecf4b3efe600a2d2d331
gunwant11 <gunwant7272@gmail.com>
2021-09-05 13:22:35 +0530

    Updated Layout

--- src/components/Blog/BlogItem.js
+++ src/components/Blog/BlogItem.js
@@ -1,16 +1,31 @@
 import classes from "./BlogItem.module.scss";
+import { useHistory } from "react-router";
 const BlogItem = (props) => {
+
+  const history = useHistory();
+  const clickHandler=(e)=>{
+    e.preventDefault();
+    history.push('/home')
+
+  }
+
   return (
     <section className={classes.bg}>
       <div className={classes.card}>
-        <div className={classes.title}>{props.post.title}</div>
+        <div className={classes.title}>
+          <p>{props.post.title}</p>
+          <img
+            onClick={clickHandler}
+            src="http://getdrawings.com/free-icon/back-button-icon-53.png"
+            width="50"
+          />
+        </div>
+
         <img className={classes.img} src={props.post.image} alt="#" />
 
         <div className={classes.description}>{props.post.description}</div>
 
-        <div className={classes.content}>
-        {props.post.content}
-        </div>
+        <div className={classes.content}>{props.post.content}</div>
       </div>
     </section>
   );
